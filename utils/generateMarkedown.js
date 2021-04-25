315b6ce803005077d03f791e601d7eca54ac7b7f
Christopher Zacharias <78880287+ChrisZ304@users.noreply.github.com>
2021-04-25 01:17:03 -0400

    Update generateMarkedown.js

--- utils/generateMarkedown.js
+++ utils/generateMarkedown.js
@@ -1,25 +1,40 @@
-function renderLicenseBadge(license) {
-    const licenseType = 
-    if (license) {
-      return ;
-    }else if (!license){
-      console.error ('no license', err)
-    }
-  }
+// function to generate markdown for README
+function generateMarkdown(answers) {
+  return  `
+  ## Title
+  ${answers.title}
+
+  ## Description 
+  ${answers.description}
   
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
-  function renderLicenseLink(license) {}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [Author Info](#author-info)
   
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
-  function renderLicenseSection(license) {}
+  ## Installation
+  ${answers.dependencies}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  ${answers.license}
+
+  ## Contributing
+  ${answers.credits}
+
+  ## Tests
+  ${answers.tests}
+
+  ## Author Info
+  ${answers.github}
+  ${answers.email}
   
-  // TODO: Create a function to generate markdown for README
-  function generateMarkdown(data) {
-    return `# ${data.title}
-  
-  `;
-  }
-  
-  module.exports = generateMarkdown;
+`;
+}
+
+module.exports = {generateMarkdown};
